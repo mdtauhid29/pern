799181d40b9f466e9c2a1f54d47f0e5775c95465
mdtauhid <tauhid2964@gmail.com>
2026-01-22 23:37:20 +0530

    implement router api methods

--- 02express/server.js
+++ 02express/server.js
@@ -4,12 +4,75 @@ const app = express();
 const port = 3000;
 const router = express.Router();
 
-app.get('/',(req,res) => {
-    res.send('assalamualaikum');
+app.use(express.json());
+
+let cars = [
+    {id:1, model:'hero', make:'honda', year:'2005'},
+    {id:2, model:'sp', make:'honda', year:'2007'},
+    {id:3, model:'apache', make:'tvs', year:'2009'}
+]
+
+app.get('/',(req,res) => { res.send('assalamualaikum'); });
+
+//get all cars
+router.get('/', (req,res) => {
+    //res.send('here is your cars');
+    res.json(cars);
 });
 
-router.get('/', (req,res) => {
-    res.send('here is your cars');
+//get a car by id
+router.get('/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const car = cars.find((car) => car.id === id);
+
+    if(!car) return res.status(404).send('car not found');
+    res.json(car);
+});
+
+//add a car
+router.post('/', (req,res) => {
+    const { model, make, year } = req.body;
+    if(!model||!make||!year){
+        return res.json('missing field');
+    }
+    const newCar = {
+        id: cars.length+1,
+        model,
+        make,
+        year:Number(year)
+    }
+
+    cars.push(newCar);
+    res.status(201).json(newCar);
+    console.log(req.body);
+});
+
+//update a car by id
+router.put('/:id',(req,res) => {
+    const id = Number(req.params.id);
+    const index = cars.findIndex(c => c.id ===id);
+
+    if(index === -1){
+        return res.status(404).send('car not found');
+    }
+    const { model, make, year} = req.body;
+    if(model) cars[index].model = model;
+    if(make) cars[index].make = make;
+    if(year) cars[index].year = year;
+    res.json(cars[index]);
+})
+
+//delete a car byb id
+router.delete('/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const index = cars.findIndex(c => c.id === id);
+
+    if(index === -1){
+        return res.status(404).send('car not found');
+    }
+
+    const deleted = cars.splice(index,1)[0];
+    res.json({message: "Car deleted", car:deleted});
 })
 
 app.use('/api/v1/cars', router);
