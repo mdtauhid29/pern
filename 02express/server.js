20a9bd17a9acec014d21676cf106dadcb860374a
mdtauhid <tauhid2964@gmail.com>
2026-01-22 23:47:13 +0530

    create a simple logging middleware

--- 02express/server.js
+++ 02express/server.js
@@ -5,6 +5,12 @@ const port = 3000;
 const router = express.Router();
 
 app.use(express.json());
+app.use((req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(`${timestamp} ${req.method} ${req.url}`);
+    next();
+})
+
 
 let cars = [
     {id:1, model:'hero', make:'honda', year:'2005'},
