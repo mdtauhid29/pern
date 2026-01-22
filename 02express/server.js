62b2e62c80a324db17370449a0ef692e6abb722b
mdtauhid <tauhid2964@gmail.com>
2026-01-22 22:27:20 +0530

    implemented router

--- 02express/server.js
+++ 02express/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 
 const app = express();
+const port = 3000;
+const router = express.Router();
 
 app.get('/',(req,res) => {
     res.send('assalamualaikum');
 });
 
-app.listen(3000, () => {
-    console.log('Server is listening on http://localhost:3000');
+router.get('/', (req,res) => {
+    res.send('here is your cars');
+})
+
+app.use('/api/v1/cars', router);
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
 });
